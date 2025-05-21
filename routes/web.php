b76cccbb9b7745287c2a289a67f940b5fbbee25a
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-05-21 23:36:34 +0100

    Day 16

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,46 @@ Route::get('/', function () {
     return view('home');
 });
 
+
+//Rota para mostrar todos os jobs
+
 Route::get('/jobs', function () {
 
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
+
+
+//Rota para mostrar a página para criar um novo job
+
+Route::get('/jobs/create', function(){
+    return view('jobs.create');
+
+});
+
+
+//Rota para mostrar um job
 
 Route::get('/job/{id}', function ($id){
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 
+});
+
+Route::post('/jobs', function(){
+   //validation
+
+    Job::create([
+        'title'=> request('title'),
+        'salary'=> request('salary'),
+        'employer_id'=>'',
+    ]);
+    return redirect('/jobs');
 });
 
 
