9b86bf4efd942ef0505d92620c24e4e365dbbacb
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-05-27 22:45:39 +0100

    Day 22

--- routes/web.php
+++ routes/web.php
@@ -71,3 +71,4 @@ Route::post('/register', [RegisterUserController::class, 'store' ]);
 
 Route::get('/login', [SessionController::class, 'create']);
 Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
