00a3805e9518c7151745647da96c60185f537d42
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-05-29 22:35:15 +0100

    Day 23 Gates

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -56,6 +56,7 @@ class JobController extends Controller
     {
 
         //authorize
+        Gate::authorize('edit-job', $job);
 
         //validade
         request()->validate([
@@ -76,6 +77,7 @@ class JobController extends Controller
     public function destroy(Job $job)
     {
         //authorize
+          Gate::authorize('edit-job', $job);
 
         //delete the job
         $job->delete();
