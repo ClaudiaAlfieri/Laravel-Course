ad248373a47fc7f6727db20da36d89bd6fbc7abc
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-05-30 16:24:02 +0100

    Update name

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Website</title>
+    <title>Job Positions</title>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
